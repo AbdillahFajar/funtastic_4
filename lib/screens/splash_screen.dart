744af2e9cdd3208df062654362999fc8c05e5463
AbdillahFajar <fajarabdillah2264@gmail.com>
2025-12-09 07:17:04 +0700

    add splash_screen.dart file

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:funtastic_4/screens/login_screen.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToDashboard();
+  }
+
+  _navigateToDashboard() async {
+    await Future.delayed(const Duration(seconds: 3));
+    if (mounted) {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+        (Route<dynamic> route) => false,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/images/GI-logo.png'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 10),
+
+            Lottie.asset(
+              'assets/animations/Material wave loading.json',
+              width: 200,
+              height: 100,
+              fit: BoxFit.contain,
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
