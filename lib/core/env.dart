0ca4274dc1e68ed7eaefb98090b1a21f66c6cf3c
AbdillahFajar <fajarabdillah2264@gmail.com>
2025-12-23 13:22:19 +0700

    upload file env.dart di dalam folder core

---
+++ lib/core/env.dart
@@ -0,0 +1,6 @@
+class Env{
+  static const supabaseUrl = 'https://dwdjvrqrimdhjpnecctv.supabase.co';
+  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3ZGp2cnFyaW1kaGpwbmVjY3R2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUzMjg2MDEsImV4cCI6MjA4MDkwNDYwMX0.D_-oTKhbt6O_QCr26PxcrK_NH7KTlIBkFKBTLlZXuiA';
+  static const storageProfileBucket = 'bucket_images';
+  static const usersCollection = 'users';
+}
