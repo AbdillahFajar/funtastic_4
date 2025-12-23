ba5bd37ee9a381848ac5f91aba97bfcd7c060942
AbdillahFajar <fajarabdillah2264@gmail.com>
2025-12-23 13:31:15 +0700

    perbarui main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:funtastic_4/screens/splash_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../core/env.dart';
+import 'domain/repositories/user_repository.dart';
+import 'domain/repositories/auth_repository.dart';
+import 'data/services/firestore_service.dart';
+import 'data/services/auth_service.dart';
+import 'presentation/providers/auth_provider.dart';
+import 'presentation/providers/profile_provider.dart';
+import 'data/services/storage_service.dart';
 import 'firebase_options.dart';
 
-void main () async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Supabase.initialize(url: Env.supabaseUrl, anonKey: Env.supabaseAnonKey);
   runApp(const MyApp());
 }
 
@@ -14,17 +26,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Funtastic 4',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Color(0xFF0D47A1)
+    final AuthRepository authRepo = AuthService();
+    final UserRepository userRepo = FirestoreService();
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider(authRepo)),
+        ChangeNotifierProvider(create: (_) => ProfileProvider(userRepo)),
+        Provider<StorageService>(create: (_) => StorageService()),
+      ],
+      child: MaterialApp(
+        title: 'Simple Notes',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF0D47A1)),
+          fontFamily: "Poppins",
         ),
-        fontFamily: "Poppins"
+        home: SplashScreen(),
       ),
-      home: SplashScreen()
     );
   }
 }
