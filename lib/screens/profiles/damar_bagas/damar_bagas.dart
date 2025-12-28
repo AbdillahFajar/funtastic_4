0d94006f7bcc3ae583215df65424d75aa6f97645
damarbagas-234 <muhammaddamar107@gmail.com>
2025-12-28 20:08:59 +0700

    memperbaharui tampilan profile

--- lib/screens/profiles/damar_bagas/damar_bagas.dart
+++ lib/screens/profiles/damar_bagas/damar_bagas.dart
@@ -6,89 +6,99 @@ class DamarBagas extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Damar Bagas ')),
+      appBar: AppBar(
+        title: Text(
+          'Damar Bagas ',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
       body: Center(
-        child: Container(
-          height: 100,
-          width: 200,
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            image: DecorationImage(
-              image: AssetImage('assets/images/Damar.jpeg'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child:Column(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
             children: [
-               Row(
-                            children: [
-                              SizedBox(
-                                width: 110,
-                                child: Text(
-                                  "NIM",
-                                  style: TextStyle(fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                              Text(" : "),
-                              Expanded(child: Text("1123150107")),
-                            ],
-                          ),
+              Container(
+                height: 350,
+                width: 300,
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/Damar.jpeg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
 
-                          SizedBox(height: 6),
+              SizedBox(height: 20),
 
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: 110,
-                                child: Text(
-                                  "Kelas",
-                                  style: TextStyle(fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                              Text(" : "),
-                              Expanded(child: Text("TI 23 SE 2")),
-                            ],
-                          ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 110,
+                    child: Text(
+                      "NIM",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Text(" : "),
+                  Expanded(child: Text("1123150107")),
+                ],
+              ),
 
-                          SizedBox(height: 6),
+              SizedBox(height: 6),
 
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: 110,
-                                child: Text(
-                                  "Minat/Keahlian",
-                                  style: TextStyle(fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                              Text(" : "),
-                              Expanded(
-                                child: Text("PBO"),
-                              ),
-                            ],
-                          ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 110,
+                    child: Text(
+                      "Kelas",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Text(" : "),
+                  Expanded(child: Text("TI 23 SE 2")),
+                ],
+              ),
 
-                          SizedBox(height: 6),
+              SizedBox(height: 6),
 
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: 110,
-                                child: Text(
-                                  "Deskripsi",
-                                  style: TextStyle(fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                              Text(" : "),
-                              Expanded(
-                                child: Text(
-                                  "Mengerjakan splash screen, tampilan tim. tampilan profile,",
-                                ),
-                              ),
-                            ],
-                          ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 110,
+                    child: Text(
+                      "Minat/Keahlian",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Text(" : "),
+                  Expanded(child: Text("PBO")),
+                ],
+              ),
+
+              SizedBox(height: 6),
+
+              Row(
+                children: [
+                  SizedBox(
+                    width: 110,
+                    child: Text(
+                      "Deskripsi",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Text(" : "),
+                  Expanded(
+                    child: Text(
+                      "Mengerjakan splash screen, tampilan tim. tampilan profile,",
+                    ),
+                  ),
+                ],
+              ),
             ],
-          ) ,
+          ),
         ),
       ),
     );
