aaf28184920ffd5247b3e26cc5e3d4315825595f
tuckerr10 <muhfrendy90@gmail.com>
2025-12-29 22:29:35 +0700

    add class of splash screen 3

---
+++ lib/screens/splash_screen_3.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'splash_screen_4.dart';
+
+class SplashScreen2 extends StatelessWidget {
+  const SplashScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
